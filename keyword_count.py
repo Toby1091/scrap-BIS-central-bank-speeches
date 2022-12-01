bb9094a6b8dc1470c18ea86536e7d76c1f76208f
Tobias Arbogast <tobias.arbogast@gmail.com>
2022-12-01 17:07:55 +0000

    Clean up script

--- keyword_count.py
+++ keyword_count.py
@@ -1,43 +1,40 @@
+"""
+
+Explain here the goal of the script, 
+Script analyzes all files in this directory and for the list of the keywords it counts the number of mentions
+
+"""
+
 import os
 
-keywords = ['natural rate', 'natural level', 'natural rate of unemployment', 'natural unemployment rate', 'u*', 'NAIRU', 
+KEYWORDS = ['natural rate', 'natural level', 'natural rate of unemployment', 'natural unemployment rate', 'u*', 'NAIRU', 
 'natural rate of interest', 'natural interest rate', 'r*', 'potential output', 'potential growth', 'Y*', 'trend growth', 
 'long-term rate of', 'neutral rate of interest', 'neutral interest rate', 'neutral real interest rate ', 
 'short-term real interest rate', 'equilibrium rate', 'output gap']
 
-txt_files = os.listdir('txt/')
+speech_txt_path = 'txt/'
 
-keyword_count_dict = {} # initialize count with 0 
+txt_file_names = os.listdir(speech_txt_path)
 
-for keyword in keywords:
-    keyword_count_dict[keyword] = 0
+keyword_count = {}
 
-#creates a new variable (in this case called "txt_file") for every element of the list txt_files
-#and runs the following code, after the loop is run, the variable is deleted from memory and the next list element is taken as a variable)
+for keyword in KEYWORDS: #creates a new variable (in this case called "keyword") for every element of the list KEYWORDS...
+    keyword_count[keyword] = 0 #...runs the following code, after loop is run, the variable is deleted from memory & the next list element is taken as a variable)
 
-for txt_file in txt_files: 
-    #print('\n', txt_file)
-    f = open('txt/' + txt_file)
-
-    print(txt_file)
+for txt_file_name in txt_file_names: 
+    f = open(speech_txt_path + txt_file_name)
 
     content = f.read()
 
-    for keyword in keywords:
-        #print(keyword + ": ", content.count(keyword))
-        #keyword_count = keyword_count + content.count(keyword)
-        keyword_count_dict[keyword] = keyword_count_dict[keyword] + content.count(keyword)
+    for keyword in KEYWORDS:
+        keyword_count[keyword] = keyword_count[keyword] + content.count(keyword)
 
-#print("\nTotal: ", keyword_count,'\n')
+for keyword in keyword_count:
+    print(keyword, ":", keyword_count[keyword])
 
-for dict_entry in keyword_count_dict:
-    print(dict_entry, ":", keyword_count_dict[dict_entry])
+total_count = 0
 
-#To compute Total from the dictionary
-total_count = 0 
-
-for dict_entry in keyword_count_dict:
-    total_count += keyword_count_dict[dict_entry] #this is a shortcut total_count = total_count + keyword_count_dict[dict_entry]
-
+for keyword in keyword_count:
+    total_count += keyword_count[keyword] #this is a shortcut total_count = total_count + keyword_count_dict[dict_entry]
 
 print('Total count: ', total_count)
