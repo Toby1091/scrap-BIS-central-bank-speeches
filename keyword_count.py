04731a0aa691ddfab1332adfae90b4ac016405ef
Tobias Arbogast <tobias.arbogast@gmail.com>
2022-12-02 19:12:34 +0000

    Added more keywords

--- keyword_count.py
+++ keyword_count.py
@@ -5,12 +5,15 @@ Script analyzes all files in this directory and for the list of the keywords it 
 
 """
 
+
+# TODO: 
+
 import os
 
-KEYWORDS = ['natural rate', 'natural level', 'natural rate of unemployment', 'natural unemployment rate', 'u*', 'NAIRU', 
-'natural rate of interest', 'natural interest rate', 'r*', 'potential output', 'potential growth', 'Y*', 'trend growth', 
-'long-term rate of', 'neutral rate of interest', 'neutral interest rate', 'neutral real interest rate ', 
-'short-term real interest rate', 'equilibrium rate', 'output gap']
+KEYWORDS = ['natural rate', 'natural level', 'natural rate of unemployment', 'natural unemployment rate', 'u*', 'N*', 'nairu', 'NIRU', 'nawru',
+'non-accelerating inflation rate of unemployment', 'noncyclical rate of unemployment', 'natural rate of interest', 'natural interest rate', 'r*', 'potential output', 
+'potential growth', 'Y*', 'trend growth', 'long-term rate of', 'long-run rate of', 'neutral rate of interest', 'neutral interest rate', 
+'neutral real interest rate ', 'short-term real interest rate', 'equilibrium rate', 'output gap']
 
 speech_directory = 'txt'
 
@@ -24,7 +27,7 @@ for keyword in KEYWORDS: # Creates a new variable (in this case called "keyword"
 for txt_file_name in txt_file_names: 
     joined_file_path = os.path.join(speech_directory, txt_file_name) # function adds a / between the two arguments
     file_handle = open(joined_file_path) # this is where we actually open the file
-    
+
     content = file_handle.read().lower()
 
     for keyword in KEYWORDS:
