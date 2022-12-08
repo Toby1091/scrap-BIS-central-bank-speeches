2c97072c1c39a44768953d7281b6a173196845e0
Tobias Arbogast <tobias.arbogast@gmail.com>
2022-12-08 23:59:41 +0000

    Use with statement when opening files to ensure that they are automatically closed when the block ends

--- keyword_count.py
+++ keyword_count.py
@@ -1,8 +1,6 @@
 """
-
-Explain here the goal of the script, 
-Script analyzes all files in this directory and for the list of the keywords it counts the number of mentions
-
+The script analyzes all files in this directory (i.e. a large data set of txt files of central bankers speeches) 
+(url: https://www.bis.org/cbspeeches/index.htm). It computes a total count of each keyword of a list of keywords across all files.
 """
 
 
@@ -14,6 +12,9 @@ KEYWORDS = ['natural rate', 'natural level', 'natural rate of unemployment', 'na
 'non-accelerating inflation rate of unemployment', 'noncyclical rate of unemployment', 'natural rate of interest', 'natural interest rate', 'r*', 'potential output', 
 'potential growth', 'Y*', 'trend growth', 'long-term rate of', 'long-run rate of', 'neutral rate of interest', 'neutral interest rate', 
 'neutral real interest rate ', 'short-term real interest rate', 'equilibrium rate', 'output gap']
+'non-accelerating inflation rate of unemployment', 'noncyclical rate of unemployment', 'natural rate of interest', 'natural interest rate', 
+'neutral interest rate', 'neutral real interest rate ', 'equilibrium real rate of interest', 'equilibrium rate', 
+'short-term real interest rate', 'output gap']
 
 speech_directory = 'txt'
 
@@ -26,9 +27,9 @@ for keyword in KEYWORDS: # Creates a new variable (in this case called "keyword"
 
 for txt_file_name in txt_file_names: 
     joined_file_path = os.path.join(speech_directory, txt_file_name) # function adds a / between the two arguments
-    file_handle = open(joined_file_path) # this is where we actually open the file
+    with open(joined_file_path) as file_handle: # this is where we actually open the file
 
-    content = file_handle.read().lower()
+        content = file_handle.read().lower()
 
     for keyword in KEYWORDS:
         keyword_count[keyword] = keyword_count[keyword] + content.count(keyword)
