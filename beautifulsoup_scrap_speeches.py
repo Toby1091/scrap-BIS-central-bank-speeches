1532e4303ca06b2e1561129d599c24e8d51b5503
Tobias Arbogast <tobias.arbogast@gmail.com>
2022-12-14 18:00:47 +0000

    Some renaming, adding bank ID to output dict

--- beautifulsoup_scrap_speeches.py
+++ beautifulsoup_scrap_speeches.py
@@ -8,6 +8,8 @@ TODO:
 - fetch ID of central banks from speech list page: https://www.bis.org/dcms/api/token_data/institutions.json?list=cbspeeches&theme=cbspeeches&
 - extract name from central bank info
 - store meta data in JSONL file
+- make debug mode automatic/configurable
+- accept cache directory as argument
 """
 
 CACHE_FOLDER = 'cache'
@@ -17,7 +19,7 @@ def get_cache_path(path):
 
 
 def fetch_page_or_pdf(path, force_refetch=False):
-    print(f'Fetch page {path} (refetch={force_refetch})')
+    print(f'Fetch page {path} (force refetch={force_refetch})')
     cache_path = get_cache_path(path)
 
     if not force_refetch and os.path.exists(cache_path):
@@ -57,7 +59,7 @@ def extract_meta_data_from_speech_list_html(html_code):
     doc_list = document.find('table', class_='documentList')
     rows = doc_list.find_all('tr')
 
-    speeches = []
+    speeches_metadata = []
     for row in rows:
         columns = row.find_all('td')
         date_column = columns[0]
@@ -65,27 +67,34 @@ def extract_meta_data_from_speech_list_html(html_code):
 
         a_tag = info_column.find('div', class_='title').find('a')
 
-        speech_data = {
+        speech_metadata = {
             'date': date_column.string.strip(),
             'title': a_tag.string.strip(),
             'path': a_tag['href'].strip(),
         }
         
-        speeches.append(speech_data)
+        speeches_metadata.append(speech_metadata)
     
-    return speeches
+    return speeches_metadata
 
 
 def extract_pdf_path_from_speech_detail_html(html_code):
     document = BeautifulSoup(html_code, 'html.parser')
     a_tag = document.find('div', id='center').find('div', class_='pdftxt').find('a', class_='pdftitle_link')
+    a_tag_bank_id_link = document.find('div', id='center').find('div', id='relatedinfo-div').find('a')
+
+    bank_id = a_tag_bank_id_link['href'].split('institutions=')[1]
+    
+    print("Central Bank ID: ", int(bank_id))
+
     path = a_tag['href']
-    return path
+
+    return path, bank_id
 
 
 def fetch_list_page(page_number, force_refetch):
     # We fetch pages in ascending order, starting with page 1 (= the oldest page). This way
-    # the contents of a certain page remain the same; new speeches will be added to the page with
+    # the contents of a certain page remain the same; new speeches_metadata will be added to the page with
     # the hightest number. This allows to easily cache results in have the script fetch only pages
     # that have been appended since the last run.
     params = f'?page={page_number}&paging_length=25&sort_list=date_asc'
@@ -99,29 +108,29 @@ def fetch_list_page(page_number, force_refetch):
 def main():
     html_code = fetch_list_page(1, True)
     page_count = extract_total_page_count_from_speech_list_html(html_code)
-
-    speeches = []
+    speeches_metadata = []
     current_page = 1
+
     while True:
         html_code = fetch_list_page(current_page, current_page == page_count)
-        speeches_of_current_page = extract_meta_data_from_speech_list_html(html_code)
-        speeches.extend(speeches_of_current_page)
         if current_page > page_count:
+        speeches_metadata_of_current_page = extract_meta_data_from_speech_list_html(html_code)
+        speeches_metadata.extend(speeches_metadata_of_current_page)
             break
         current_page += 1
 
-
-    for index, speech in enumerate(speeches):
+    for index, speech in enumerate(speeches_metadata):
         print(index)
         if speech['path'].endswith('.pdf'):
             fetch_page_or_pdf(speech['path'])
         else:
             fetch_page_or_pdf(speech['path'])
             html_code = read_file_from_cache(speech['path'])
-            pdf_path = extract_pdf_path_from_speech_detail_html(html_code)
-            fetch_page_or_pdf(pdf_path)
+            pdf_path_and_bank_id = extract_pdf_path_from_speech_detail_html(html_code)
+            fetch_page_or_pdf(pdf_path_and_bank_id[0])
+            bank_id = pdf_path_and_bank_id[1]
+            speech['central_bank_id'] = bank_id
 
-
-    pprint.pprint(speeches)
+    pprint.pprint(speeches_metadata)
 
 main()
