cb5d08f1cacefa55a3108df71beb98b49afa47b8
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2022-12-31 13:32:33 +0100

    Move input / output files and directories into subfolders

--- config.py
+++ config.py
@@ -2,16 +2,16 @@
 PAGE_SIZE = 25
 
 # Directory in which to store / cache downloaded files (will be written in)
-CACHE_DIR = 'cache'
+CACHE_DIR = 'output/cache'
 
 # Directory in which to store text files containing the content extracted from PDFs
-TXT_DIR = 'textified_pdfs'
+TXT_DIR = 'output/textified_pdfs'
 
 # JSON file with metadata about speeches extracted from HTML (will be created / overwritten)
-RESULT_FILE = 'result.json'
+RESULT_FILE = 'output/result.json'
 
 # Text file that will be read with additional bank names and mapped bank names (will only be read)
-BANK_NAMES_FILE = 'list_of_missing_bank_names.txt'
+BANK_NAMES_FILE = 'input/list_of_missing_bank_names.txt'
 
 # Text file that will be read with keywords to search (will only be read)
-KEYWORDS_FILE = 'list_of_keywords.txt'
+KEYWORDS_FILE = 'input/list_of_keywords.txt'
