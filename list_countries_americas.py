7cf8803519c6188f5400ecaa9cba51f770b9e296
Tobias Arbogast <tobias.arbogast@gmail.com>
2023-01-18 13:26:18 +0100

    List of all American and only Spanish-speaking countries

---
+++ list_countries_americas.py
@@ -0,0 +1,14 @@
+list_all_countries = ['Antigua and Barbuda', 'Argentina', 'Bahamas', 'Barbados', 'Belize', 'Bolivia', 'Brazil', 'Canada', 'Chile', 'Colombia', 'Costa Rica', 'Cuba', 'Dominica', 'Dominican Republic', 'Ecuador', 'El Salvador', 'Grenada', 'Guatemala', 'Guyana', 'Haiti', 'Honduras', 'Jamaica', 'Mexico', 'Nicaragua', 'Panama', 'Paraguay', 'Peru', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'Suriname', 'Trinidad and Tobago', 'United States of America', 'Uruguay', 'Venezuela']
+
+list_spanish_countries = ['Argentina', 'Bolivia', 'Chile', 'Colombia', 'Costa Rica', 'Cuba', 'Dominican Republic', 'Ecuador', 'El Salvador', 'Equatorial Guinea', 'Guatemala', 'Honduras', 'Mexico', 'Nicaragua', 'Panama', 'Paraguay', 'Peru', 'Uruguay', 'Venezuela', 'Puerto Rico']
+
+other_countries = ['Antigua and Barbuda', 'Bahamas', 'Barbados', 'Belize', 'Brazil', 'Canada', 'Dominica', 'Grenada', 'Guyana', 'Haiti', 'Jamaica', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'Suriname', 'Trinidad and Tobago', 'United States of America']
+
+# print(f'All countries ({len(list_all_countries)}):', list_all_countries)
+# print(f'Other countries ({len(other_countries)}):', other_countries)
+
+print('Spanish-speaking countries: ', len(list_spanish_countries))
+for country in list_spanish_countries:
+    print(country)
+
+
