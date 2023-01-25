a9bd0fcff037e9103b9a3f082c71c8d10ff35d42
Tobias Arbogast <tobias.arbogast@gmail.com>
2023-01-25 10:55:17 +0100

    Write count of each keyword for each speech to json

--- speech_analysis/keyword_count.py
+++ speech_analysis/keyword_count.py
@@ -1,9 +1,8 @@
 """
 The script analyzes all files in this directory (i.e. a large data set of txt files of central bankers speeches) 
-(url: https://www.bis.org/cbspeeches/index.htm). It computes a total count of each keyword of a list of keywords across all files.
+(url: https://www.bis.org/cbspeeches/index.htm). It counts the mentions of each keyword for each file.
 """
 
-from config import KEYWORDS_FILE, TXT_DIR
 
 # TODO: 
 # - make script insensitive to accents (using regular expressions), lower case etc.
@@ -12,40 +11,31 @@ from config import KEYWORDS_FILE, TXT_DIR
 # - implement a switch to count only keywords where count > 10
 
 import os
+import json
 
-with open(KEYWORDS_FILE) as file_handle:
+with open('speech_analysis/list_of_keywords.txt') as file_handle:
     content = file_handle.read()
 
 KEYWORDS = [s for s in content.split('\n') if s]
 
-txt_file_names = os.listdir(TXT_DIR)
+txt_file_names = os.listdir('output/textified_pdfs')
 
-keyword_count = {}
-
-for keyword in KEYWORDS: # Creates a new variable (in this case called "keyword") for every element of the list KEYWORDS...
-    keyword_count[keyword] = 0 #...runs the following code, after loop is run, the variable is deleted from memory & the next list element is taken as a variable)
+keyword_count_by_speech = {}
 
 for txt_file_name in txt_file_names: 
-    joined_file_path = os.path.join(TXT_DIR, txt_file_name) # function adds a / between the two arguments
+    joined_file_path = os.path.join('output/textified_pdfs', txt_file_name) # function adds a / between the two arguments
     with open(joined_file_path) as file_handle: # this is where we actually open the file
 
         content = file_handle.read().lower().replace('\n', ' ')
 
+    keywords = {}
+
     for keyword in KEYWORDS:
-        keyword_count[keyword] = keyword_count[keyword] + content.count(keyword)
+        count = content.count(keyword)
+        if count > 0:
+            keywords[keyword] = count
 
-for keyword in keyword_count: 
-    if keyword_count[keyword] > 10: # This is a switch to only print keywords that occur more than 10 times
-        print(keyword, ":", keyword_count[keyword])
+    keyword_count_by_speech[txt_file_name] = keywords
 
-total_count = 0
-
-for keyword in keyword_count:
-    total_count += keyword_count[keyword] #This is a shortcut total_count = total_count + keyword_count_dict[dict_entry]
-
-# print('Total count: ', total_count)
-# print()
-# print()
-# print(repr(keyword_count))
-# print(type(keyword_count))
-# print(keyword_count)
+with open('output/keyword_by_speech_output.json', 'w') as f:
+    json.dump(keyword_count_by_speech, f, indent=4)
