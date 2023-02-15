2d83135704d07c62f8a8fa94e3a168aaa0682e99
Tobias Arbogast <tobias.arbogast@gmail.com>
2023-02-15 12:02:27 +0100

    Update speech_count_by_cb

--- speech_analysis/speech_count_by_cb.py
+++ speech_analysis/speech_count_by_cb.py
@@ -2,17 +2,17 @@
 import json
 import pprint
 
-from config import RESULT_FILE
+# from config import RESULT_FILE
 
-with open(RESULT_FILE) as file_handle:
+with open('output/speech_metadata.json') as file_handle:
     speech_metadata = json.loads(file_handle.read())
 
 uncounted_cb_list = []
 cb_count_dict = {'Missing': 0}
 
 for dict in speech_metadata:
-    if 'central_bank' in dict:
-        central_bank_name = dict['central_bank']
+    if 'bank_name' in dict or  dict['bank_name'] is None:
+        central_bank_name = dict['bank_name']
 
         if central_bank_name in cb_count_dict:
             cb_count_dict[central_bank_name] += 1
@@ -22,4 +22,11 @@ for dict in speech_metadata:
         cb_count_dict['Missing'] += 1
 
 pprint.pprint(cb_count_dict)
+
+with open('output/keyword_by_speech_all.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        headers = ['bank', 'speech count']
+        headers.extend(keywords)
+        writer.writerow(headers)
+
 
